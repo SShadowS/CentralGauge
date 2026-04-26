6e452322e3488848eceaa9a815d400fb54bfbc6b
Torben Leth <SShadowS@SShadowS.dk>
2026-04-27 01:54:26 +0200

    feat(site): FamilyBadge domain widget (link to /families/:slug)

---
+++ site/src/lib/components/domain/FamilyBadge.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Tag from '$lib/components/ui/Tag.svelte';
+  interface Props { slug: string; }
+  let { slug }: Props = $props();
+</script>
+
+<a class="link" href="/families/{slug}">
+  <Tag variant="neutral">{slug}</Tag>
+</a>
+
+<style>
+  .link { text-decoration: none; }
+  .link:hover { text-decoration: none; }
+</style>
