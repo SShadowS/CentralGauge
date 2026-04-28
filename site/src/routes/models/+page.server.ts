81def1897e373da4ad10b61209b1e7aab3eb1323
Torben Leth <SShadowS@SShadowS.dk>
2026-04-28 15:23:17 +0200

    feat(site): /models +page.server.ts loader (passthrough filters + dep tag)

---
+++ site/src/routes/models/+page.server.ts
@@ -0,0 +1,17 @@
+import type { PageServerLoad } from './$types';
+import type { ModelsIndexResponse } from '$shared/api-types';
+import { passthroughLoader } from '$lib/server/loader-helpers';
+
+const inner = passthroughLoader<ModelsIndexResponse>({
+  depTag: 'app:models',
+  fetchPath: '/api/v1/models',
+  resultKey: 'models',
+});
+
+export const load: PageServerLoad = async (event) => ({
+  ...(await inner(event)),
+  filters: {
+    family:   event.url.searchParams.get('family') ?? '',
+    has_runs: event.url.searchParams.get('has_runs') ?? '',
+  },
+});
