6f1eca42d0a845f6e26e5919ab504d76b1c53bd3
Torben Leth <SShadowS@SShadowS.dk>
2026-05-03 05:45:39 +0200

    refactor(container/pwsh-session): private fields, add PwshSessionError import

--- src/container/pwsh-session.ts
+++ src/container/pwsh-session.ts
@@ -4,6 +4,8 @@
  * @module container/pwsh-session
  */
 
+import { PwshSessionError } from "../errors.ts";
+
 export interface PwshSessionOptions {
   /** Recycle after this many execute() calls. Default 100. */
   recycleThreshold?: number;
@@ -47,24 +49,24 @@ $bcContainerHelperConfig.usePwshForBc24 = $false
 export class PwshContainerSession {
   private _state: SessionState = "dead";
   private _callCount = 0;
-  protected process: SpawnedProcess | null = null;
-  protected readonly recycleThreshold: number;
-  protected readonly defaultTimeoutMs: number;
-  protected readonly bootstrapTimeoutMs: number;
-  protected readonly bootstrapScript: string;
-  protected readonly spawnFactory: () => SpawnedProcess;
+  private _process: SpawnedProcess | null = null;
+  private readonly _recycleThreshold: number;
+  private readonly _defaultTimeoutMs: number;
+  private readonly _bootstrapTimeoutMs: number;
+  private readonly _bootstrapScript: string;
+  private readonly _spawnFactory: () => SpawnedProcess;
 
   constructor(
     public readonly containerName: string,
     options: PwshSessionOptions = {},
   ) {
-    this.recycleThreshold = options.recycleThreshold ??
+    this._recycleThreshold = options.recycleThreshold ??
       DEFAULT_RECYCLE_THRESHOLD;
-    this.defaultTimeoutMs = options.defaultTimeoutMs ?? DEFAULT_TIMEOUT_MS;
-    this.bootstrapTimeoutMs = options.bootstrapTimeoutMs ??
+    this._defaultTimeoutMs = options.defaultTimeoutMs ?? DEFAULT_TIMEOUT_MS;
+    this._bootstrapTimeoutMs = options.bootstrapTimeoutMs ??
       DEFAULT_BOOTSTRAP_TIMEOUT_MS;
-    this.bootstrapScript = options.bootstrapScript ?? DEFAULT_BOOTSTRAP_SCRIPT;
-    this.spawnFactory = options.spawnFactory ?? defaultSpawnFactory;
+    this._bootstrapScript = options.bootstrapScript ?? DEFAULT_BOOTSTRAP_SCRIPT;
+    this._spawnFactory = options.spawnFactory ?? defaultSpawnFactory;
   }
 
   get state(): SessionState {
@@ -80,24 +82,48 @@ export class PwshContainerSession {
   }
 
   get shouldRecycle(): boolean {
-    return this._callCount >= this.recycleThreshold;
+    return this._callCount >= this._recycleThreshold;
   }
 
   // To be implemented in later tasks:
   init(): Promise<void> {
-    throw new Error("not implemented");
+    throw new PwshSessionError(
+      "init not yet implemented",
+      "session_init_failed",
+      {
+        container: this.containerName,
+        recycleThreshold: this._recycleThreshold,
+        defaultTimeoutMs: this._defaultTimeoutMs,
+        bootstrapTimeoutMs: this._bootstrapTimeoutMs,
+        bootstrapScript: this._bootstrapScript,
+        hasProcess: this._process !== null,
+        hasSpawnFactory: this._spawnFactory !== undefined,
+      },
+    );
   }
 
   execute(_script: string, _timeoutMs?: number): Promise<ExecuteResult> {
-    throw new Error("not implemented");
+    throw new PwshSessionError(
+      "execute not yet implemented",
+      "session_state_violation",
+      { container: this.containerName },
+    );
   }
 
   recycle(): Promise<void> {
-    throw new Error("not implemented");
+    throw new PwshSessionError(
+      "recycle not yet implemented",
+      "session_recycle_failed",
+      { container: this.containerName },
+    );
   }
 
   dispose(): Promise<void> {
-    throw new Error("not implemented");
+    throw new PwshSessionError(
+      "dispose not yet implemented",
+      "session_init_failed",
+      { container: this.containerName },
+    );
   }
 }
 
